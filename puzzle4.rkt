6bc11c2ee680c29d03e205e8fcad92f57a4c0458
Jack Firth <jackhfirth@gmail.com>
2019-12-04 23:33:05 -0800

    Solve day 4 part 2

--- puzzle4.rkt
+++ puzzle4.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
-(require fancy-app
+(require (for-syntax racket/base)
+         fancy-app
          racket/match
          racket/pretty
          racket/stream
@@ -8,7 +9,8 @@
          rebellion/collection/list
          rebellion/streaming/reducer
          rebellion/streaming/transducer
-         rebellion/type/record)
+         rebellion/type/record
+         rebellion/type/tuple)
 
 (module+ test
   (require rackunit))
@@ -33,50 +35,49 @@
     (check-equal? (bound-digits 256310) (list 2 5 6 3 1 0))
     (check-equal? (bound-digits 732736) (list 7 3 2 7 3 6))))
 
-(define-record-type state (digits candidates))
+(define-tuple-type state (a b c d e f) #:constructor-name constructor:state)
+
+(define-match-expander state
+  (syntax-rules ()
+    [(_ a b c d e f)
+     (? state?
+        (app state-a a)
+        (app state-b b)
+        (app state-c c)
+        (app state-d d)
+        (app state-e e)
+        (app state-f f))])
+  (make-rename-transformer #'constructor:state))
 
 (define (next s)
-  (match-define (list a b c d e f) (state-digits s))
-  (define candidates (state-candidates s))
-
-  (define (skip a b c d e f)
-    (present (state #:digits (list a b c d e f) #:candidates candidates)))
-  
+  (match-define (state a b c d e f) s)
   (cond
-    [(> a b) (skip a a a a a a)]
-    [(> b c) (skip a b b b b b)]
-    [(> c d) (skip a b c c c c)]
-    [(> d e) (skip a b c d d d)]
-    [(> e f) (skip a b c d e e)]
-    [else
-     (define candidates*
-       (if (or (equal? a b) (equal? b c) (equal? c d) (equal? d e) (equal? e f))
-           (add1 candidates)
-           candidates))
-     (define (include a b c d e f)
-       (present
-        (state #:digits (list a b c d e f) #:candidates candidates*)))
-     (cond
-       [(< f 9) (include a b c d e (add1 f))]
-       [(< e 9)
-        (define e* (add1 e))
-        (include a b c d e* e*)]
-       [(< d 9)
-        (define d* (add1 d))
-        (include a b c d* d* d*)]
-       [(< c 9)
-        (define c* (add1 c))
-        (include a b c* c* c* c*)]
-       [(< b 9)
-        (define b* (add1 b))
-        (include a b* b* b* b* b*)]
-       [(< a 9)
-        (define a* (add1 a))
-        (include a* a* a* a* a* a*)]
-       [else absent])]))
+    [(> a b) (present (state a a a a a a))]
+    [(> b c) (present (state a b b b b b))]
+    [(> c d) (present (state a b c c c c))]
+    [(> d e) (present (state a b c d d d))]
+    [(> e f) (present (state a b c d e e))]
+    [(< f 9) (present (state a b c d e (add1 f)))]
+    [(< e 9)
+     (define e* (add1 e))
+     (present (state a b c d e* e*))]
+    [(< d 9)
+     (define d* (add1 d))
+     (present (state a b c d* d* d*))]
+    [(< c 9)
+     (define c* (add1 c))
+     (present (state a b c* c* c* c*))]
+    [(< b 9)
+     (define b* (add1 b))
+     (present (state a b* b* b* b* b*))]
+    [(< a 9)
+     (define a* (add1 a))
+     (present (state a* a* a* a* a* a*))]
+    [else absent]))
 
 (define (start-state lower-bound)
-  (state #:digits (bound-digits lower-bound) #:candidates 0))
+  (match-define (list a b c d e f) (bound-digits lower-bound))
+  (state a b c d e f))
 
 (define (in-unfold f init)
   (let loop ([opt (present init)])
@@ -91,29 +92,29 @@
                  (taking 23)
                  #:into into-list))
     (define expected
-      (list (state #:digits (list 2 5 6 3 1 0) #:candidates 0)
-            (state #:digits (list 2 5 6 6 6 6) #:candidates 0)
-            (state #:digits (list 2 5 6 6 6 7) #:candidates 1)
-            (state #:digits (list 2 5 6 6 6 8) #:candidates 2)
-            (state #:digits (list 2 5 6 6 6 9) #:candidates 3)
-            (state #:digits (list 2 5 6 6 7 7) #:candidates 4)
-            (state #:digits (list 2 5 6 6 7 8) #:candidates 5)
-            (state #:digits (list 2 5 6 6 7 9) #:candidates 6)
-            (state #:digits (list 2 5 6 6 8 8) #:candidates 7)
-            (state #:digits (list 2 5 6 6 8 9) #:candidates 8)
-            (state #:digits (list 2 5 6 6 9 9) #:candidates 9)
-            (state #:digits (list 2 5 6 7 7 7) #:candidates 10)
-            (state #:digits (list 2 5 6 7 7 8) #:candidates 11)
-            (state #:digits (list 2 5 6 7 7 9) #:candidates 12)
-            (state #:digits (list 2 5 6 7 8 8) #:candidates 13)
-            (state #:digits (list 2 5 6 7 8 9) #:candidates 14)
-            (state #:digits (list 2 5 6 7 9 9) #:candidates 14)
-            (state #:digits (list 2 5 6 8 8 8) #:candidates 15)
-            (state #:digits (list 2 5 6 8 8 9) #:candidates 16)
-            (state #:digits (list 2 5 6 8 9 9) #:candidates 17)
-            (state #:digits (list 2 5 6 9 9 9) #:candidates 18)
-            (state #:digits (list 2 5 7 7 7 7) #:candidates 19)
-            (state #:digits (list 2 5 7 7 7 8) #:candidates 20)))
+      (list (state 2 5 6 3 1 0)
+            (state 2 5 6 6 6 6)
+            (state 2 5 6 6 6 7)
+            (state 2 5 6 6 6 8)
+            (state 2 5 6 6 6 9)
+            (state 2 5 6 6 7 7)
+            (state 2 5 6 6 7 8)
+            (state 2 5 6 6 7 9)
+            (state 2 5 6 6 8 8)
+            (state 2 5 6 6 8 9)
+            (state 2 5 6 6 9 9)
+            (state 2 5 6 7 7 7)
+            (state 2 5 6 7 7 8)
+            (state 2 5 6 7 7 9)
+            (state 2 5 6 7 8 8)
+            (state 2 5 6 7 8 9)
+            (state 2 5 6 7 9 9)
+            (state 2 5 6 8 8 8)
+            (state 2 5 6 8 8 9)
+            (state 2 5 6 8 9 9)
+            (state 2 5 6 9 9 9)
+            (state 2 5 7 7 7 7)
+            (state 2 5 7 7 7 8)))
     (check-equal? actual expected)))
 
 (define-syntax each<=?
@@ -137,21 +138,35 @@
     (check-false (each<=? [8 5] [0 9]))))
 
 (define (state-in-bound? s #:upper upper-bound)
-  (match-define (list x1 x2 x3 x4 x5 x6) (state-digits s))
+  (match-define (state x1 x2 x3 x4 x5 x6) s)
   (match-define (list u1 u2 u3 u4 u5 u6) upper-bound)
   (each<=? [x1 u1] [x2 u2] [x3 u3] [x4 u4] [x5 u5] [x6 u6]))
 
+(define (valid-state? s)
+  (match-define (state a b c d e f) s)
+  (define a=b? (equal? a b))
+  (define b=c? (equal? b c))
+  (define c=d? (equal? c d))
+  (define d=e? (equal? d e))
+  (define e=f? (equal? e f))
+  (or (and a=b? (not b=c?))
+      (and b=c? (not a=b?) (not c=d?))
+      (and c=d? (not b=c?) (not d=e?))
+      (and d=e? (not c=d?) (not e=f?))
+      (and e=f? (not d=e?))))
+
 (define (num-solutions lower-bound upper-bound)
-  (define upper-digits (bound-digits (add1 upper-bound)))
+  (define upper-digits (bound-digits upper-bound))
   (transduce (in-unfold next (start-state lower-bound))
              (taking-while (state-in-bound? _ #:upper upper-digits))
-             (mapping state-candidates)
-             #:into (reducer-map into-last #:range present-value)))
+             (filtering valid-state?)
+             #:into into-count))
 
 (module+ test
   (test-case "num-solutions"
-    (check-equal? (num-solutions 111111 111111) 1)
-    (check-equal? (num-solutions 223450 223450) 0)
-    (check-equal? (num-solutions 111110 111112) 2)))
+    (check-equal? (num-solutions 112233 112233) 1)
+    (check-equal? (num-solutions 123444 123444) 0)
+    (check-equal? (num-solutions 123445 123449) 5)
+    (check-equal? (num-solutions 111122 111122) 1)))
 
 (num-solutions 256310 732736)
